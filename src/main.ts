f3cfc62d053b9c1f2566a0b2ed6e0a1110e5469f
Sebastián Corrales <laboratorioaunacol@hotmail.com>
2025-01-30 09:45:44 -0500

    feat: cambio mensaje del puerto de ejecución

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
 
   await app.listen(3000);
   
-  console.log('Aplicación corriendo en puerto SCS!!!: ', 3000 );
+  console.log('Aplicación corriendo en puerto SCS!!! : ', 3000 );
 }
 bootstrap();
